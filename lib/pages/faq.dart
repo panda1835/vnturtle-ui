5510eaeae2ebd332b9a7ec3c03323059298875be
panda1835 <lehoangphuc1820@gmail.com>
2023-09-09 18:46:56 +0700

    show a blank page if there's error reading the JSON file

--- lib/pages/faq.dart
+++ lib/pages/faq.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
-
 class CauHoiThuongGapPage extends StatefulWidget {
   @override
   _CauHoiThuongGapPageState createState() => _CauHoiThuongGapPageState();
@@ -17,7 +16,7 @@ class _CauHoiThuongGapPageState extends State<CauHoiThuongGapPage> {
   }
 
   Future<void> loadFaqData() async {
-    String jsonData = await DefaultAssetBundle.of(context).loadString('faq.json');
+    String jsonData = await DefaultAssetBundle.of(context).loadString('content/faq.json');
     setState(() {
       faqData = json.decode(jsonData)['FAQs'];
     });
@@ -26,6 +25,48 @@ class _CauHoiThuongGapPageState extends State<CauHoiThuongGapPage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    
+    Widget buildContent() {
+      if (faqData.isEmpty) {
+        return SizedBox.shrink(); // Return an empty widget
+      } else {
+        return Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Câu Hỏi Thường Gặp',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: faqData.length,
+                itemBuilder: (context, index) {
+                  return ExpansionTile(
+                    title: Text(faqData[index]['question']),
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(faqData[index]['answer']),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      }
+    }
+
     return Scaffold(
       backgroundColor: theme.secondaryHeaderColor,
       appBar: AppBar(
@@ -38,37 +79,10 @@ class _CauHoiThuongGapPageState extends State<CauHoiThuongGapPage> {
         ),
         backgroundColor: theme.secondaryHeaderColor,
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Câu Hỏi Thường Gặp',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: faqData.length,
-              itemBuilder: (context, index) {
-                return ExpansionTile(
-                  title: Text(faqData[index]['question']),
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(faqData[index]['answer']),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
-        ],
+      body: Visibility(
+        visible: faqData.isNotEmpty,
+        child: buildContent(),
       ),
     );
-
   }
 }
