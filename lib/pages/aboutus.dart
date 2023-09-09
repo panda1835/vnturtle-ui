ce55d1d1a5629d5c691281c71acb5b967f42e95d
panda1835 <lehoangphuc1820@gmail.com>
2023-09-09 18:47:28 +0700

    create about us page that show the content from markdown file

---
+++ lib/pages/aboutus.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class VeChungToiPage extends StatefulWidget {
+  @override
+  _VeChungToiPageState createState() => _VeChungToiPageState();
+}
+
+class _VeChungToiPageState extends State<VeChungToiPage> {
+  Future<String> _loadAboutUsContent() async {
+    return await rootBundle.loadString('content/about_us.md');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: theme.secondaryHeaderColor,
+      appBar: AppBar(
+        title: Text(
+          'VNTURTLE', 
+          style: TextStyle(
+            fontFamily: "Happy Monkey",
+            color: theme.primaryColor,
+          ),
+        ),
+        backgroundColor: theme.secondaryHeaderColor,
+      ),
+      body: FutureBuilder(
+        future: _loadAboutUsContent(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Failed to load about us content.'
+                ),
+              );
+            } else {
+              return Markdown(data: snapshot.data!);
+            }
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
