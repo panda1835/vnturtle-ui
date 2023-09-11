1b0b66bb31a1af31fbda626c05f989e5d4f5faca
panda1835 <lehoangphuc1820@gmail.com>
2023-09-11 09:58:23 +0700

    add the following change to species page:

--- lib/pages/species_info.dart
+++ lib/pages/species_info.dart
@@ -2,156 +2,131 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
-class ThongTinLoaiPage extends StatelessWidget {
+class ThongTinLoaiPage extends StatefulWidget {
+  @override
+  _ThongTinLoaiPageState createState() => _ThongTinLoaiPageState();
+}
+
+class _ThongTinLoaiPageState extends State<ThongTinLoaiPage> {
+  List<String> speciesNames = [];
+  List<String> filteredSpeciesNames = [];
+  Map<String, dynamic> speciesData = {};
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadSpeciesData();
+  }
+
+  Future<void> loadSpeciesData() async {
+    String jsonString =
+        await DefaultAssetBundle.of(context).loadString('content/species_info.json');
+    speciesData = jsonDecode(jsonString)['species'];
+
+    setState(() {
+      speciesNames = speciesData.keys.toList();
+      filteredSpeciesNames = speciesNames;
+    });
+  }
+
+  void filterSpeciesList(String query) {
+    setState(() {
+      filteredSpeciesNames = speciesNames
+          .where((name) => name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: DefaultAssetBundle.of(context).loadString('content/species_info.json'),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> speciesData = jsonDecode(snapshot.data.toString())['species'];
-
-          List<String> speciesNames = speciesData.keys.toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Species List'),
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: theme.secondaryHeaderColor,
+      appBar: AppBar(
+        backgroundColor: theme.secondaryHeaderColor,
+        title: Text(
+          'VNTURTLE',
+          style: TextStyle(
+            fontFamily: "Happy Monkey",
+            color: theme.primaryColor,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Thông Tin Loài',
+              style: TextStyle(
+                fontSize: 24.0, 
+                fontWeight: FontWeight.bold),
             ),
-            body: ListView.builder(
-              itemCount: speciesNames.length,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              controller: searchController,
+              onChanged: filterSpeciesList,
+              decoration: InputDecoration(
+                hintText: 'Tìm kiếm',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredSpeciesNames.length,
               itemBuilder: (context, index) {
-                String speciesName = speciesNames[index];
+                String speciesName = filteredSpeciesNames[index];
                 Map<String, dynamic> speciesInfo = speciesData[speciesName];
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailedSpeciesPage(speciesData: speciesInfo),
+                return Container(
+                  margin: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: theme.secondaryHeaderColor,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(speciesInfo['reference_images'][0]),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            height: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                image: AssetImage(speciesInfo['reference_images'][0]),
-                                fit: BoxFit.cover,
-                              ),
+                      SizedBox(width: 16.0),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              speciesInfo['name_vi'],
+                              style:
+                                  TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                             ),
-                          ),
+                            SizedBox(height: 8.0),
+                            Text('Tình trạng bảo tồn: ${speciesInfo['conservation_status']}'),
+                            SizedBox(height: 8.0),
+                            Text('Tên tiếng Anh: ${speciesInfo['name_en']}'),
+                          ],
                         ),
-                        SizedBox(width: 16.0),
-                        Expanded(
-                          flex: 3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                speciesInfo['name_vi'],
-                                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 8.0),
-                              Text('Tình trạng bảo tồn: ${speciesInfo['conservation_status']}'),
-                              SizedBox(height: 8.0),
-                              Text('Tên Tiếng Anh: ${speciesInfo['name_en']}'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 );
               },
             ),
-          );
-        } else {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Species List'),
-            ),
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
-    );
-  }
-}
-
-class DetailedSpeciesPage extends StatelessWidget {
-  final Map<String, dynamic> speciesData;
-
-  DetailedSpeciesPage({required this.speciesData});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(speciesData['name_vi']),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
-          Text(
-            'Đặc điểm nhận dạng',
-            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 8.0),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: speciesData['identification_images']
-                  .map<Widget>((image) => Image.asset(
-                        image,
-                        width: 200,
-                        height: 200,
-                        fit: BoxFit.cover,
-                      ))
-                  .toList(),
-            ),
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            'Hình tham khảo',
-            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 8.0),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: speciesData['reference_images']
-                  .map<Widget>((image) => Image.asset(
-                        image,
-                        width: 200,
-                        height: 200,
-                        fit: BoxFit.cover,
-                      ))
-                  .toList(),
-            ),
-          ),
-          SizedBox(height: 16.0),
-          Text('Conservation Status: ${speciesData['conservation_status']}'),
-          SizedBox(height: 8.0),
-          Text('English Name: ${speciesData['name_en']}'),
-          SizedBox(height: 8.0),
-          Text('Distribution: ${speciesData['distribution']}'),
-          SizedBox(height: 8.0),
-          Text('Fun Fact: ${speciesData['fun_fact']}'),
         ],
       ),
     );
