33f21400eacc4bd91ff0fae935530942115e1688
panda1835 <lehoangphuc1820@gmail.com>
2023-09-28 17:27:36 +0700

    add language switch to faq page

--- lib/pages/faq_page.dart
+++ lib/pages/faq_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
+import 'package:vnturtle/widgets/language_switch.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class CauHoiThuongGapPage extends StatefulWidget {
   @override
   _CauHoiThuongGapPageState createState() => _CauHoiThuongGapPageState();
@@ -8,33 +11,49 @@ class CauHoiThuongGapPage extends StatefulWidget {
 
 class _CauHoiThuongGapPageState extends State<CauHoiThuongGapPage> {
   List<dynamic> faqData = [];
+  String curentLocale = '';
 
   @override
   void initState() {
     super.initState();
-    loadFaqData();
   }
 
-  Future<void> loadFaqData() async {
-    String jsonData = await DefaultAssetBundle.of(context).loadString('content/faq.json');
-    setState(() {
-      faqData = json.decode(jsonData)['FAQs'];
-    });
+  Future<List<dynamic>> loadFaqData(String locale) async {
+    String jsonData = await DefaultAssetBundle.of(context).loadString('content/faq_$locale.json');
+    return json.decode(jsonData)['FAQs'];
   }
 
   @override
   Widget build(BuildContext context) {
     
-    Widget buildContent() {
-      if (faqData.isEmpty) {
-        return SizedBox.shrink(); // Return an empty widget
-      } else {
-        return Column(
+    if (curentLocale != Localizations.localeOf(context).languageCode){
+      setState(() {
+        curentLocale = Localizations.localeOf(context).languageCode;
+      });
+      loadFaqData(curentLocale).then((value) => setState(() {
+        faqData = value;
+      },));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'VNTURTLE', 
+        ),
+        centerTitle: true,
+        actions: [
+          LanguageSwitchWidget(),
+          const SizedBox(width: 12,)
+        ],
+      ),
+      body: Visibility(
+        visible: faqData.isNotEmpty,
+        child: Column(
           children: [
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Câu Hỏi Thường Gặp',
+                AppLocalizations.of(context)!.fAQ,
                 style: TextStyle(
                   fontSize: 20.0,
                   fontWeight: FontWeight.bold,
@@ -62,19 +81,7 @@ class _CauHoiThuongGapPageState extends State<CauHoiThuongGapPage> {
               ),
             ),
           ],
-        );
-      }
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'VNTURTLE', 
         ),
-      ),
-      body: Visibility(
-        visible: faqData.isNotEmpty,
-        child: buildContent(),
       ),
     );
   }
