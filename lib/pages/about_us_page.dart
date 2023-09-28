b39b72fc63c5f26b1b78f9afc1982dd4f8e49425
panda1835 <lehoangphuc1820@gmail.com>
2023-09-28 17:34:13 +0700

    add langugage switch to aboutus page

--- lib/pages/about_us_page.dart
+++ lib/pages/about_us_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:vnturtle/widgets/language_switch.dart';
 
 class VeChungToiPage extends StatefulWidget {
   @override
@@ -8,20 +9,29 @@ class VeChungToiPage extends StatefulWidget {
 }
 
 class _VeChungToiPageState extends State<VeChungToiPage> {
-  Future<String> _loadAboutUsContent() async {
-    return await rootBundle.loadString('content/about_us.md');
+  String currentLocale = '';
+  Future<String> _loadAboutUsContent(String locale) async {
+    return await rootBundle.loadString('content/about_us_$locale.md');
   }
 
   @override
   Widget build(BuildContext context) {
+    if (currentLocale != Localizations.localeOf(context).languageCode){
+      currentLocale = Localizations.localeOf(context).languageCode;
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'VNTURTLE', 
         ),
+        centerTitle: true,
+        actions: [
+          LanguageSwitchWidget(),
+          const SizedBox(width: 12,)
+        ],
       ),
       body: FutureBuilder(
-        future: _loadAboutUsContent(),
+        future: _loadAboutUsContent(currentLocale),
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasError) {
