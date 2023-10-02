7f88d36906de53e8d2736020aeebcf0577e1b7e0
panda1835 <lehoangphuc1820@gmail.com>
2023-10-02 09:07:32 +0700

    add language switch

--- lib/pages/detailed_species_page.dart
+++ lib/pages/detailed_species_page.dart
@@ -1,34 +1,53 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:vnturtle/widgets/language_switch.dart';
 
 // ignore: must_be_immutable
-class DetailedSpeciesPage extends StatelessWidget {
+class DetailedSpeciesPage extends StatefulWidget {
   final String speciesName;
-  Map<String, dynamic> speciesData = {};
 
   DetailedSpeciesPage({required this.speciesName});
 
-  Future<Map<String, dynamic>> loadSpeciesData() async {
+  @override
+  State<DetailedSpeciesPage> createState() => _DetailedSpeciesPageState();
+}
+
+class _DetailedSpeciesPageState extends State<DetailedSpeciesPage> {
+  Map<String, dynamic> speciesData = {};
+
+  String currentLocale = "";
+
+  Future<Map<String, dynamic>> loadSpeciesData(String locale) async {
     String jsonString =
-        await rootBundle.loadString('content/species_info_vi.json');
+        await rootBundle.loadString('content/species_info_$locale.json');
     return jsonDecode(jsonString);
   }
 
-
   @override
   Widget build(BuildContext context) {
+    if (currentLocale != Localizations.localeOf(context).languageCode){
+      setState(() {
+        currentLocale = Localizations.localeOf(context).languageCode;
+      });
+      
+    }
     return FutureBuilder(
-      future: loadSpeciesData(),
+      future: loadSpeciesData(currentLocale),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.hasData) {
-          Map<String, dynamic> speciesInfo = snapshot.data[speciesName];
+          Map<String, dynamic> speciesInfo = snapshot.data[widget.speciesName];
           // Use the 'jsonData' variable to access the JSON data
           return Scaffold(
             appBar: AppBar(
-              title: Text(
+              title: const Text(
                 'VNTURTLE', 
               ),
+              centerTitle: true,
+              actions: const [
+                LanguageSwitchWidget(),
+                SizedBox(width: 12,)
+              ],
             ),
             body: ListView(
               children: [
@@ -37,13 +56,34 @@ class DetailedSpeciesPage extends StatelessWidget {
                     padding: const EdgeInsets.all(16.0),
                     child: Text(
                       speciesInfo['primary_name'],
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontSize: 24.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
+              
+                SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: speciesInfo['reference_images']
+                          .map<Widget>(
+                            (image) => Image.asset(
+                              image,
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: Text(
@@ -61,36 +101,6 @@ class DetailedSpeciesPage extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                     child: Row(
                       children: speciesInfo['identification_images']
-                          .map<Widget>(
-                            (image) => Image.asset(
-                              image,
-                              width: 200,
-                              height: 200,
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    'Hình tham khảo',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8.0),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: speciesInfo['reference_images']
                           .map<Widget>(
                             (image) => Image.asset(
                               image,
@@ -146,9 +156,4 @@ class DetailedSpeciesPage extends StatelessWidget {
       },
     );
   }
-
-  // Widget build(BuildContext context) {
-    
-    
-  // }
 }
