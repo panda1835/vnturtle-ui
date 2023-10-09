faed50beba1d678ca40e82cc6cf7132a71b62177
panda1835 <lehoangphuc1820@gmail.com>
2023-10-09 16:16:34 +0700

    put confirm page into a container of 400px width

--- lib/pages/confirm_page.dart
+++ lib/pages/confirm_page.dart
@@ -70,7 +70,7 @@ class _ConfirmPageState extends State<ConfirmPage> {
       backgroundColor: Theme.of(context).primaryColor,
       content: Text(
         snackBarMessage,
-        style: TextStyle(color: Colors.white),
+        style: const TextStyle(color: Colors.white),
       ),
       duration: const Duration(seconds: 5),
     );
@@ -101,136 +101,141 @@ class _ConfirmPageState extends State<ConfirmPage> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage(widget.speciesInfo['reference_images'][0]), // Replace with your image path
-              ),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                widget.speciesInfo['primary_name'][currentLocale],
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: theme.primaryColor,
+        child: Center(
+          child: Container(
+            width: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: CircleAvatar(
+                    radius: 100,
+                    backgroundImage: AssetImage(widget.speciesInfo['reference_images'][0]), // Replace with your image path
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              widget.speciesInfo['scientific_name'],
-              style: const TextStyle(
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.only(left: 10, right: 10),
-              width: 400,
-              child: RichText(
-                text: TextSpan(
-                  text: widget.speciesInfo['warning'][currentLocale],
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    widget.speciesInfo['primary_name'][currentLocale],
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: theme.primaryColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  widget.speciesInfo['scientific_name'],
                   style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
+                    fontStyle: FontStyle.italic,
                   ),
-                  children: [
-                    TextSpan(
-                      text: ' ${AppLocalizations.of(context)!.contactHotline} ',
-                      style: const TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const TextSpan(
-                      text: '18001522',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.red,
-                      ),
-                    ),
-                    TextSpan(
-                      text: ' ${AppLocalizations.of(context)!.forAssistance}.',
-                      style: const TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
                 ),
-              ),
-            ),
-            
-            const Divider(
-              height: 20,
-              thickness: 1,
-              indent: 10.0,
-              endIndent: 10.0,
-              color: Colors.grey,
-            ),
-
-            Container(
-              width: double.infinity,
-              child: Card(
-                elevation: 5,
-                margin: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        AppLocalizations.of(context)!.contributeToVNTurtle,
-                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                const SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  // width: 400,
+                  child: RichText(
+                    text: TextSpan(
+                      text: widget.speciesInfo['warning'][currentLocale],
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        AppLocalizations.of(context)!.contributeYourImage,
-                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      _isContributeLoading
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 200,
-                            child: ElevatedButton(
-                              onPressed: _isContributed ? null : _contributeImage,
-                              child: Text(AppLocalizations.of(context)!.contributeImage),
-                            ),
+                      children: [
+                        TextSpan(
+                          text: ' ${AppLocalizations.of(context)!.contactHotline} ',
+                          style: const TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
                           ),
-                        ],
-                      ),
-                      
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      // if (_isReported)
-                      Text(
-                        AppLocalizations.of(context)!.contributeImageMessage,
-                        style: TextStyle(fontWeight: FontWeight.normal,),
-                      )
-                    ],
+                        ),
+                        const TextSpan(
+                          text: '18001522',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.red,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' ${AppLocalizations.of(context)!.forAssistance}.',
+                          style: const TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ),            
-          ],
+                
+                SizedBox(height: 10,),
+
+                const Divider(
+                  height: 20,
+                  thickness: 1,
+                  indent: 10.0,
+                  endIndent: 10.0,
+                  color: Colors.grey,
+                ),
+                SizedBox(height: 10,),
+          
+                Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.all(8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          AppLocalizations.of(context)!.contributeToVNTurtle,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.contributeYourImage,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        _isContributeLoading
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 200,
+                              child: ElevatedButton(
+                                onPressed: _isContributed ? null : _contributeImage,
+                                child: Text(AppLocalizations.of(context)!.contributeImage),
+                              ),
+                            ),
+                          ],
+                        ),
+                        
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        // if (_isReported)
+                        Text(
+                          AppLocalizations.of(context)!.contributeImageMessage,
+                          style: TextStyle(fontWeight: FontWeight.normal,),
+                        )
+                      ],
+                    ),
+                  ),
+                ),            
+              ],
+            ),
+          ),
         ),
       ),
     );
