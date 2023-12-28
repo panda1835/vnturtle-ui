d855e49928fb326715544b86839669694c6d7ce2
panda1835 <lehoangphuc1820@gmail.com>
2023-12-28 23:05:41 +0700

    add manual guide page

---
+++ lib/pages/manual_guide_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:vnturtle/widgets/language_switch.dart';
+
+class ManualGuidePage extends StatefulWidget {
+  @override
+  _ManualGuidePageState createState() => _ManualGuidePageState();
+}
+
+class _ManualGuidePageState extends State<ManualGuidePage> {
+  String currentLocale = '';
+  
+  Future<String> _loadManualGuideContent(String locale) async {
+    return await rootBundle.loadString('content/manual-guide/manual_guide_$locale.md');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentLocale != Localizations.localeOf(context).languageCode){
+      setState(() {
+        currentLocale = Localizations.localeOf(context).languageCode;
+      });
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'VNTURTLE', 
+        ),
+        centerTitle: true,
+        actions: [
+          LanguageSwitchWidget(),
+          const SizedBox(width: 12,)
+        ],
+      ),
+      body: FutureBuilder(
+        future: _loadManualGuideContent(currentLocale),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Failed to load about us content.'
+                ),
+              );
+            } else {
+              return Markdown(data: snapshot.data!);
+            }
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
